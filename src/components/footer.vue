3c300a8637d7d9ac8c5e0a050a196156d66b3831
lirong <lirong@hnnewsoft.com>
2023-12-14 11:24:36 +0800

    修改 footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -8,7 +8,8 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12">
-                        Copyright © {{ new Date().getFullYear() }} OpenCity Community. All rights reserved. 开放城市共同体.
+                      <!-- Copyright © {{ new Date().getFullYear() }} OpenCity Community. All rights reserved. 开放城市共同体. -->
+                        Copyright © {{ new Date().getFullYear() }} 深圳雨本信息技术有限公司
                         <a href="https://beian.miit.gov.cn/" target="_blank" class="text-dark">粤ICP备17123203号</a>
                     </div>
                 </div>
